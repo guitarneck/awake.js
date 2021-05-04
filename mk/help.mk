2a6d7093b35ca4a6184d49ccecea4a1e84b9cd53
guitarneck@free.fr <guitarneck@free.fr>
2021-05-04 20:53:23 +0200

    new help rule for makefile

---
+++ mk/help.mk
@@ -0,0 +1,15 @@
+# List the rules with additional comments inside a Makefile
+#
+# example:
+# --------
+# watch: ## watch unit tests
+#	@watch "make test --silent"
+#
+# Inlude in makefile like this to prevent default running:
+#
+# ifeq ($(MAKECMDGOALS),help)
+# -include mk/help.mk
+# endif
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(firstword $(MAKEFILE_LIST)) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
